99328a7eb111e01c4cb3ce36a31003203e914e9b
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2017-09-18 18:08:47 -0400

    .

--- Cosmos-DB-Graph-Trigger/PushDocs/FormatEdge.js
+++ Cosmos-DB-Graph-Trigger/PushDocs/FormatEdge.js
@@ -1,14 +1,27 @@
-function trigger() {
+function trigger(){
     var context = getContext();
     var request = context.getRequest();
-
+    
     //  Document to be created in the current operation
     var documentToCreate = request.getBody();
 
     //  Validate the document is an edge:
     //  Does it have a label?
-    if ('label' in documentToCreate) {
-        documentToCreate.isEdge = true
+    if('label' in documentToCreate){
+        var properties = [
+            "name",
+            "age",
+            "country"
+        ];
+
+        for(let p of properties){
+            var obj = new Object;
+
+            obj._value = documentToCreate[p];
+            obj.id = "1";
+
+            documentToCreate[p] = [obj];
+        }
     }
 
     //  Update the document that will be created
--- Cosmos-DB-Graph-Trigger/PushDocs/Program.cs
+++ Cosmos-DB-Graph-Trigger/PushDocs/Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace PushDocs
 {
     class Program
     {
+        #region DB Connections
+        private const string SERVICE_ENDPOINT = "https://vpl.documents.azure.com:443/";
+        private const string AUTH_KEY = "unFCqdUzrb3QUIk2JHeimJznnzmwnt8DOw5TGRsueudmoNSCFCtrttIM7j773qPhQn67agAnd9ottAPHNplDow==";
+        private const string DATABASE = "mydb";
+        private const string COLLECTION = "mycoll";
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InsertDocumentsAsync().Wait();
+        }
+
+        private static async Task InsertDocumentsAsync()
+        {
+            var client = new DocumentClient(new Uri(SERVICE_ENDPOINT), AUTH_KEY);
+            var collectionUri = UriFactory.CreateDocumentCollectionUri(DATABASE, COLLECTION);
+            var james = GetEmbeddedResource("Edge-James.json");
+            var laura = GetEmbeddedResource("Edge-Laura.json");
+            var phil = GetEmbeddedResource("Edge-Phil.json");
+
+            foreach (var obj in new[] { james, laura, phil })
+            {
+                await client.CreateDocumentAsync(
+                    collectionUri,
+                    obj,
+                    new RequestOptions { PreTriggerInclude = new[] { "formatEdge" } });
+            }
+
+            Console.WriteLine(james);
+        }
+
+        private static object GetEmbeddedResource(string name)
+        {
+            var assembly = typeof(Program).GetTypeInfo().Assembly;
+
+            using (var stream = assembly.GetManifestResourceStream("PushDocs." + name))
+            using (var streamReader = new StreamReader(stream))
+            using (var jsonReader = new JsonTextReader(streamReader))
+            {
+                var serializer = new JsonSerializer();
+                var obj = serializer.Deserialize(jsonReader);
+
+                return obj;
+            }
         }
     }
 }
