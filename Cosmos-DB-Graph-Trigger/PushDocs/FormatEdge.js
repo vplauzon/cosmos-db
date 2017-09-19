8a186de5cdd4b6d12657a55c0280b8f88d3a2c78
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2017-09-19 10:06:45 -0400

    .

--- Cosmos-DB-Graph-Trigger/PushDocs/FormatEdge.js
+++ Cosmos-DB-Graph-Trigger/PushDocs/FormatEdge.js
@@ -1,26 +1,25 @@
-function trigger(){
+function trigger() {
     var context = getContext();
     var request = context.getRequest();
-    
+
     //  Document to be created in the current operation
     var documentToCreate = request.getBody();
 
     //  Validate the document is an edge:
     //  Does it have a label?
-    if('label' in documentToCreate){
-        var properties = [
-            "name",
-            "age",
-            "country"
-        ];
+    if ('label' in documentToCreate) {
+        for (let p of Object.keys(documentToCreate)) {
+            //  Exclude non-custom properties
+            if (!p.startsWith('_')
+                && p != "id"
+                && p != "label") {
+                var obj = new Object;
 
-        for(let p of properties){
-            var obj = new Object;
+                obj._value = documentToCreate[p];
+                obj.id = "1";
 
-            obj._value = documentToCreate[p];
-            obj.id = "1";
-
-            documentToCreate[p] = [obj];
+                documentToCreate[p] = [obj];
+            }
         }
     }
 
