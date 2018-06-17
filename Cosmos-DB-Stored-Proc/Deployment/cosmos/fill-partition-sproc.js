0cbdafb21c937f55e4bf6545a0460d1f791d1523
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-06-17 07:49:18 -0400

    .

--- Cosmos-DB-Stored-Proc/Deployment/cosmos/fill-partition-sproc.js
+++ Cosmos-DB-Stored-Proc/Deployment/cosmos/fill-partition-sproc.js
@@ -1,5 +1,44 @@
-﻿function sproc1() {
-  var response = getContext().getResponse();
+﻿function createRecords(partitionKey, recordCount) {
+    var context = getContext();
+    var collection = context.getCollection();
+    var recordCreated = 0;
 
-  response.setBody(JSON.stringify(42));
+    for (i = 0; i < recordCount; i++) {
+        var randomString = "";
+
+        for (j = 0; j < 1024; ++j) {
+            randomString += String.fromCharCode(Math.random() * 90 + 33);
+        }
+
+        var documentToCreate = {
+            part: partitionKey,
+            name: randomString.substr(0, 64),
+            profile: {
+                age: Math.round(Math.random() * 100),
+                salary: Math.round(Math.random() * 10000000) / 100,
+                project: randomString.substr(64, 128)
+            },
+            alias: {
+                name: randomString.substr(64, 256),
+                reference: randomString.substr(300, 256)
+            }
+        };
+
+        var accepted = collection.createDocument(
+            collection.getSelfLink(),
+            documentToCreate,
+            function (err, documentCreated) {
+                if (err) {
+                    throw new Error('Error' + err.message);
+                }
+                else {
+                    ++recordCreated;
+                }
+            });
+
+        if (!accepted)
+            return;
+    }
+
+    context.getResponse().setBody(recordCreated);
 }
